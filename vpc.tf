9c644fa9f35a2e3c3cf9df436207a400d411cf6d
manithams <manithams90@gmail.com>
2018-07-22 12:29:36 +0530

    Add subnet

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,18 @@ variable "cluster-name" {
   type = "string"
   }
 
-resource "aws_vpc" "smava-vpc" {
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "smava-eks-vpc" {
   cidr_block = "10.0.0.0/16"
   }
+
+resource "aws_subnet" "smava-eks-subnet" {
+  count = 2
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  cidr_block = "10.0.${count.index}.0/24"
+  vpc_id = "${aws_vpc.smava-eks-vpc.id}"
+}
+
+
 
