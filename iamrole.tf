18eaa74e897de3c214685029b55fca05d577b6e1
manithams <manithams90@gmail.com>
2018-07-22 16:15:48 +0530

    iam-roles for eks

---
+++ iamrole.tf
@@ -0,0 +1,27 @@
+resource "aws_iam_role" "smava-eks-iam-role" {
+  name = "smava-eks-iam-role"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect":"Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+        "Action": "sts:AssumeRole"
+     }
+   ]
+}
+  POLICY
+}
+resource "aws_iam_role_policy_attachment" "smava-eks-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role = "${aws_iam_role.smava-eks-iam-role.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "smava-eks-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role = "${aws_iam_role.smava-eks-iam-role.name}"
+}
+
