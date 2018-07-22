fa7b420aa2e61d46839f4cc784102efe9a271243
manithams <manithams90@gmail.com>
2018-07-22 17:45:16 +0530

    awsconfig change and Change vpc cidr to match eks RFC1918 requirement

--- awsconfig.tf
+++ awsconfig.tf
@@ -1,11 +1,17 @@
+variable "access_key" {}
+variable "secret_key" {}
+
+
 provider "aws" {
-  region = "us-east-1"     
+  region = "us-east-1"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 }
 
 variable "account_owner" {
   default = "520447751155"
 }
 
-variable "access_key" {}
 
-variable "secret_key" {}
+
+
--- vpc.tf
+++ vpc.tf
@@ -6,20 +6,20 @@ variable "cluster-name" {
 data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "smava-vpc" {
-  cidr_block = "20.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
   }
 
 resource "aws_subnet" "smava-az1-subnets" {
   count = 2
   availability_zone = "${data.aws_availability_zones.available.names[0]}"
-  cidr_block = "20.0.${count.index+1}.0/24"
+  cidr_block = "10.0.${count.index+1}.0/24"
   vpc_id = "${aws_vpc.smava-vpc.id}"
 }
 
 resource "aws_subnet" "smava-az2-subnets" {
   count = 2
   availability_zone = "${data.aws_availability_zones.available.names[1]}"
-  cidr_block = "20.0.${count.index+3}.0/24"
+  cidr_block = "10.0.${count.index+3}.0/24"
   vpc_id = "${aws_vpc.smava-vpc.id}"
 }
 
