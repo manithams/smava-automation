4ed3ff5b268e7dfdf958fb0752a8908dd7e85487
manithams <manithams90@gmail.com>
2018-07-22 12:41:30 +0530

    Expand vpc to have 2 AZs

--- vpc.tf
+++ vpc.tf
@@ -9,12 +9,17 @@ resource "aws_vpc" "smava-eks-vpc" {
   cidr_block = "10.0.0.0/16"
   }
 
-resource "aws_subnet" "smava-eks-subnet" {
+resource "aws_subnet" "smava-az1-subnets" {
   count = 2
-  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
   cidr_block = "10.0.${count.index}.0/24"
   vpc_id = "${aws_vpc.smava-eks-vpc.id}"
 }
 
-
+resource "aws_subnet" "smava-az2-subnets" {
+  count = 2
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+  cidr_block = "10.0.${count.index}.0/24"
+  vpc_id = "${aws_vpc.smava-eks-vpc.id}"
+}
 
