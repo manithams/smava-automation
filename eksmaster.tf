0c14e5861ddc011ae432579df26b6f12348c677c
manithams <manithams90@gmail.com>
2018-07-23 00:21:41 +0530

    Add dependencies for eks-master

--- eksmaster.tf
+++ eksmaster.tf
@@ -5,4 +5,9 @@ resource "aws_eks_cluster" "smava-eks-cluster" {
     security_group_ids =  ["${aws_security_group.smava-eks-master-sg.id}"]
     subnet_ids = ["${aws_subnet.smava-az1-subnets.*.id[0]}","${aws_subnet.smava-az2-subnets.*.id[0]}"]
   }
+  depends_on = [
+    "aws_iam_role_policy_attachment.smava-eks-AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.smava-eks-AmazonEKSServicePolicy",
+    "aws_internet_gateway.smava-igw",
+  ]
 }
